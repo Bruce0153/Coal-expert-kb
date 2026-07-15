a22466523025bba90b5354326a54d56aa2a9e63f
Zeng, Hualong <140960050+Bruce0153@users.noreply.github.com>
2026-07-15 17:10:05 +0800

    refactor: remove empty complex utility package

--- src/coal_kb/complex_qa/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""导出复杂科学问答的文档处理公共函数。"""
-
-from coal_kb.complex_qa.utils.documents import (
-    clone_plan_for_subquery,
-    deduplicate_documents,
-    tag_documents,
-)
-
-__all__ = ["clone_plan_for_subquery", "deduplicate_documents", "tag_documents"]

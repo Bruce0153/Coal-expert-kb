754b6b993a145c6e21775cc9ffeca146a084886c
Zeng, Hualong <140960050+Bruce0153@users.noreply.github.com>
2026-07-16 14:34:19 +0800

    feat: export ablation suite interfaces

--- src/coal_kb/research/__init__.py
+++ src/coal_kb/research/__init__.py
@@ -6,15 +6,27 @@ from coal_kb.research.graph import GraphRoute
 from coal_kb.research.models import ExperimentSpec, ResearchRoute, RouteResult
 from coal_kb.research.multimodal import MultimodalRoute
 from coal_kb.research.service import ResearchRouteService
+from coal_kb.research.suites import (
+    AblationSuiteRunner,
+    ExperimentSuiteConfig,
+    ExperimentVariant,
+    SuiteRun,
+    apply_route_parameters,
+)
 
 __all__ = [
+    "AblationSuiteRunner",
     "AgentAction",
     "ControlledAgentRoute",
     "ExperimentSpec",
+    "ExperimentSuiteConfig",
+    "ExperimentVariant",
     "GraphRoute",
     "MultimodalRoute",
     "ResearchExperiment",
     "ResearchRoute",
     "ResearchRouteService",
     "RouteResult",
+    "SuiteRun",
+    "apply_route_parameters",
 ]
